780079817f9dd5edc87ec2ca855adb6749c60cdd
Yi Zhang <zhangyiant@hotmail.com>
2016-10-09 21:29:14 +0800

    Complete StockClosedTransaction class.

--- src/main/java/org/anteestudio/stock/StockClosedTransaction.java
+++ src/main/java/org/anteestudio/stock/StockClosedTransaction.java
@@ -6,10 +6,10 @@ public class StockClosedTransaction {
     private Long transId;
     private String symbol;
     private float buyPrice;
-    //private Date buyDate;
-    //private float sellPrice;
-    //private Date sellDate;
-    //private Long quantity;
+    private Date buyDate;
+    private float sellPrice;
+    private Date sellDate;
+    private Long quantity;
 
     public StockClosedTransaction() {
     }
@@ -32,5 +32,33 @@ public class StockClosedTransaction {
     }
     public void setBuyPrice(float buyPrice) {
 	this.buyPrice = buyPrice;
+    }
+    public Date getBuyDate() {
+	return buyDate;
+    }
+    public void setBuyDate(Date buyDate) {
+	this.buyDate = buyDate;
+	return;
+    }
+    public float getSellPrice() {
+	return sellPrice;
+    }
+    public void setSellPrice(float sellPrice) {
+	this.sellPrice = sellPrice;
+	return;
+    }
+    public Date getSellDate() {
+	return sellDate;
+    }
+    public void setSellDate(Date sellDate) {
+	this.sellDate = sellDate;
+	return;
+    }
+    public Long getQuantity() {
+	return quantity;
+    }
+    public void setQuantity(Long quantity) {
+	this.quantity = quantity;
+	return;
     }
 }
