b1eaa128965b7568d6a6ffb0c4916c96274a4593
Yi Zhang <zhangyiant@hotmail.com>
2016-11-06 21:10:27 +0800

    Add a sample Spring bean.

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.List;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,6 +10,8 @@ import org.anteestudio.stock.StockClosedTransaction;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import java.io.PrintWriter;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -24,11 +27,16 @@ public class HelloWorldServlet extends HttpServlet {
 
 	PrintWriter out = resp.getWriter();
 
+	ServletContext ctx = this.getServletContext();
+	WebApplicationContext wtx = WebApplicationContextUtils.getWebApplicationContext(ctx);
+	NewHelloWorld newHelloWorld = wtx.getBean("newHelloWorld", NewHelloWorld.class);
+
 	out.println("<html>");
 	out.println("<head><title>My Servlet</title></head>");
 	out.println("<link href='bootstrap-3.3.7-dist/css/bootstrap.min.css' rel='stylesheet'>");
 		    
 	out.println("<body>");
+	out.println(newHelloWorld.getMessage());
 	out.println("<table class='table table-striped'>");
 
 	List<StockClosedTransaction> result =
---
+++ src/main/java/NewHelloWorld.java
@@ -0,0 +1,11 @@
+public class NewHelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+	this.message = message;
+    }
+
+    public String getMessage() {
+	return this.message;
+    }
+}
