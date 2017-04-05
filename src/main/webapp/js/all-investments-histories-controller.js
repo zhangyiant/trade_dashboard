a6f0df8f6db8548d4b84a812f76c7ab984fa7ca4
Yi Zhang <zhangyiant@hotmail.com>
2017-04-06 00:29:35 +0800

    Add trends picture.

--- src/main/webapp/js/all-investments-histories-controller.js
+++ src/main/webapp/js/all-investments-histories-controller.js
@@ -1,10 +1,43 @@
 var app = angular.module("myApp", []);
 app.controller('myCtrl', function($scope, $http) {
+    function updateChart () {
+        var data = new Array();
+        for (var counter = 0;
+             counter < $scope.allInvestmentsHistories.length;
+             counter++) {
+            var point = new Array();
+            var d = $scope.allInvestmentsHistories[counter].epochMilli;
+            var v = $scope.allInvestmentsHistories[counter].totalValue;
+            point.push(d);
+            point.push(v);
+            data.push(point);
+        }
+        data.reverse();
+        var chart = Highcharts.chart("trends", {
+            title: {
+                text: "History Investment Trends"
+            },
+            yAxix: {
+                title: {
+                    text: "Total Value"
+                }
+            },
+            xAxis: {
+                type: "datetime"
+            },
+            series: [{
+                name: "value trends",
+                data: data
+                }]
+        });
+        return;
+    }
     $http({
         method: "GET",
         url: "rest/all-investments-histories.json"
     }).then(function successCallback(response) {
         $scope.allInvestmentsHistories = response.data;
+        updateChart();
     }, function errorCallback(response) {
     });
 });
