2242b4941992738365de5a399c8b11de5e0da6e3
Yi Zhang <zhangyiant@hotmail.com>
2017-04-11 23:01:35 +0800

    Add symbol filtering for stock transaction table.

--- src/main/webapp/js/stock-transactions-controller.js
+++ src/main/webapp/js/stock-transactions-controller.js
@@ -2,18 +2,40 @@ var app = angular.module("myApp", []);
 app.controller('myCtrl', function($scope, $http) {
 
     $scope.getData = function() {
+        var params = {};
+        params.period = $scope.period;
+        if ($scope.symbol !== "all") {
+            params.symbol = $scope.symbol;
+        }
         $http({
             method: "GET",
             url: "rest/stock-transactions.json",
-            params: {period: $scope.period}
+            params: params
         }).then(function successCallback(response) {
             $scope.stockTransactions = response.data;
         }, function errorCallback(response) {
         });
         return;
     };
+    function updateStockInfos() {
+        $http({
+            method: "GET",
+            url: "rest/stock-infos.json"
+        }).then(function successCallback(response) {
+            $scope.stockInfos = response.data;
+            var all_symbol = {
+                name: "All stocks",
+                symbol: "all"
+            };
+            $scope.stockInfos.push(all_symbol);
+            $scope.symbol = "all";
+            $scope.getData();
+        }, function errorCallback(responsse) {
+        });
+        return;
+    }
     $scope.period="1month";
-    $scope.getData();
+    updateStockInfos();
 });
 app.filter("epochMilliToDatetimeString", function () {
     return function(epochMilli) {
