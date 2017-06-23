1b67e29448bc2a1ca27b58da4f106903310a8b99
Yi Zhang <zhangyiant@hotmail.com>
2017-06-23 21:18:14 +0800

    Update stock price page to show lowest transaction data.

--- src/main/webapp/js/stock-price-controller.js
+++ src/main/webapp/js/stock-price-controller.js
@@ -12,7 +12,36 @@ app.controller('myCtrl', function($scope, $http) {
                  "601857",
                  "601998"];
 
-  $scope.stockPrices = [];      
+  $scope.stockPrices = [];
+
+  function updateTransactionPrice(symbol, data) {
+    var params = {
+      symbol: symbol,
+      sort: true,
+      period: "all"
+    };
+    $http({
+      method: "GET",
+      url: "rest/stock-transactions.json",
+      params: params
+    }).then(function successCallback(response) {
+      var counter = 0;
+      var transactionData = response.data;
+      for (counter = 0; counter < transactionData.length; counter++) {
+        if (counter == 0) {
+          data.buyPrice1 = transactionData[0].price;
+          data.buyQuantity1 = transactionData[0].quantity;
+        } else if (counter == 1) {
+          data.buyPrice2 = transactionData[1].price;
+          data.buyQuantity2 = transactionData[1].quantity;
+        } else {
+          break;
+        }
+      }
+    }, function errorCallback(response) {
+    });
+  }
+
   $scope.getData = function() {
     var counter;
     for (counter = 0; counter < symbols.length; counter++) {
@@ -20,11 +49,14 @@ app.controller('myCtrl', function($scope, $http) {
       $http({
         method: "GET",
         url: "spring/rest/stock-price",
-        params: {symbol: symbol}
+        params: {symbol: symbol},
+        symbol: symbol
       }).then(function successCallback(response) {
         var data = response.data;
+        var symbol = response.config.symbol;
         data.strDatetime = (new Date(data.updateDatetime.epochSecond*1000)).toString();
         $scope.stockPrices.push(data);
+        updateTransactionPrice(symbol, data);
       }, function errorCallback(response) {
       });
     }
